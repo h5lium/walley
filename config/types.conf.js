7993e1e7b44344242c28ca0663f8ca0751725b25
h5lium <h5lium@163.com>
2013-09-10 23:17:33 +0800

    templates

---
+++ config/types.conf.js
@@ -0,0 +1,9 @@
+
+
+module.exports = [{
+	title: '主页', name: 'home'	// required
+}, {
+	title: '最新动态', name: 'tweet', plural: 'tweets'
+}, {
+	title: '关于', name: 'about'
+}];
---
+++ controllers/List.js
@@ -0,0 +1,51 @@
+var BaseController = require("./Base"),
+	View = require("../views/Base"),
+	model = new (require("../models/ContentModel")),
+	_ = require('underscore');
+
+module.exports = BaseController.extend({ 
+	content: null,
+	run: function(query, req, res, next) {
+		model.setDB(req.db);
+		var menuMarkup = _.reduce(global.config.types, function(memo, type) {
+        	if (type.name === 'home') {
+        		return memo + '<li><a href="/">'+ type.title +'</a></li>';
+        	}
+        	return memo + '<li><a href="/'+ (type.plural || type.name) +'">'+ type.title +'</a></li>';
+        }, '');
+		
+		var self = this;
+		self.getContent(query, function(innerMarkup) {
+			res.render('inner', {
+				inner: innerMarkup
+			}, function(err, bodyMarkup) {
+				var v = new View(res, 'main');
+				v.render({
+					menu: menuMarkup,
+					body: bodyMarkup
+				});
+			});
+		});
+	},
+	getContent: function(query, callback) {
+		var self = this;
+		var blogArticles = '';
+		model.getlist(function(err, records) {
+			if(records.length > 0) {
+				for(var i=0; record=records[i]; i++) {
+					var record = records[i];
+					blogArticles += '\
+						<section class="item">\
+                            <img src="' + record.picture + '" alt="" />\
+                            <h2>' + record.title + '</h2>\
+                            <p>' + record.text + '</p>\
+                            <br class="clear" />\
+							<hr />\
+                        </section>\
+					';
+				}
+			}
+			callback(blogArticles);
+		}, query);
+	}
+});
